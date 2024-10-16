ddeaa297acc776d2beb8ae40837d502709251dd1
Jashu-codings <jaswanthsunny11@gmail.com>
2024-10-16 23:10:40 +0530

    changes commited to oops1st branch

--- src/com/jashu/OOPS/basic_oops.java
+++ src/com/jashu/OOPS/basic_oops.java
@@ -2,6 +2,18 @@ package com.jashu.OOPS;
 
 public class basic_oops {
     public static void main(String[] args) {
+        int[] array = new int[5];
+        array[0] = 12;
+        array[1] = 11;
+        array[2] = 56;
+        array[3] = 87;
+        array[4] = 76;
+
+        System.out.println(array[2] + " got the element on 2nd indices or 3rd size element");
+
+
+
+
 
 
     }
